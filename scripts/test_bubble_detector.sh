7740280077e7f767123c11ca17d2d6a2f53347c4
kvidie <duongquockhanha8@gmail.com>
2024-05-14 14:58:03 +0700

    Updated

--- scripts/test_bubble_detector.sh
+++ scripts/test_bubble_detector.sh
@@ -1,1 +1,6 @@
-python3 bubble_text_detector.py --image dataset/GarakutayaManta --weight /home/doki/Downloads/comic-speech-bubble-detector-640.onnx --device cuda:0 --output TEST --save-output
+python3 bubble_text_detector.py /
+    --image dataset/GarakutayaManta /
+    --weight /home/doki/Downloads/comic-speech-bubble-detector-640.onnx /
+    --device cuda:0 /
+    --output TEST /
+    --save-output

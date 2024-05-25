075ec8b1fb75609449c77bc1c07a0f5581961812
kvidie <duongquockhanha8@gmail.com>
2024-05-25 12:05:48 +0700

    Use argosmodel

--- modules/translate/translate.py
+++ modules/translate/translate.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import translators as ts
+import argostranslate.package
+import argostranslate.translate
 import pickle
+import pathlib
+from transformers import MarianMTModel, MarianTokenizer
+from typing import Sequence
+from modules.utils import *
 import time
 
-def translate(data):
+def translate(args, data):
     '''
     Return:
         {
@@ -22,7 +28,30 @@ def translate(data):
         ...
         }
     '''
+    # print("enter Translate")
+
     start_time = time.time()
+    argostranslate.package.update_package_index()
+    available_packages = argostranslate.package.get_available_packages()
+    available_package = list(
+    filter(
+        lambda x: x.from_code == 'ja' and x.to_code == 'en', available_packages
+        )
+    )[0]
+    
+    download_path = available_package.download()
+    argostranslate.package.install_from_path(download_path)
+    package_path = pathlib.Path('checkpoints/translate-en_vi-1_2.argosmodel')
+    argostranslate.package.install_from_path(package_path)
+    installed_languages = argostranslate.translate.get_installed_languages()
+    from_lang = list(filter(
+        lambda x: x.code == 'ja',
+        installed_languages))[0]
+    to_lang = list(filter(
+        lambda x: x.code == 'vi',
+        installed_languages))[0]
+    translation = from_lang.get_translation(to_lang)
+
     for key, value in data.items():
         temp_list = []
         for key, small_value in value['bubbles'].items():
@@ -30,16 +59,25 @@ def translate(data):
         if len(temp_list) == 0:
             continue #don't need to translate an empty list
         temp_text = "\n".join(temp_list)
-        trans_text = ts.translate_text(temp_text, translator = "bing", from_language = 'ja', to_language='vi')
+        try:
+            trans_text = ts.translate_text(temp_text, translator = "bing", from_language = 'ja', to_language='vi')
+        except:
+            trans_text = translation.translate(temp_text)
+        # trans_text = argostranslate.translate.translate(temp_text, 'ja', 'vi')
         temp_text = ""
         trans_list = trans_text.split('\n')
+        # print(trans_text)
         if len(trans_list) != len(temp_list):
             print('translate wrong', len(trans_list), len(temp_list))
-            continue
-        counter = 0
-        for key, small_value in value['bubbles'].items():
-            small_value['trs_text'] = trans_list[counter]
-            counter += 1
-    print(f'Translating time: {time.time() - start_time:.3f} seconds')
-
+            for key, small_value in value['bubbles'].items():
+                small_value['trs_text'] = small_value['text']
+                counter += 1
+        else:
+            counter = 0
+            for key, small_value in value['bubbles'].items():
+                small_value['trs_text'] = trans_list[counter]
+                counter += 1
+    
+    print('Translate time:', time.time() - start_time)
+    # print('end translate')
     return data
